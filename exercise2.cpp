262e9e43fd0de50eac261c7fd9155aa1075a2a34
mobhani <zainmobhani_b@outlook.com>
2023-02-10 17:37:53 -0500

    changed exercise1 from vector to array

--- exercise2.cpp
+++ exercise2.cpp
@@ -4,32 +4,32 @@ Zain Mobhani
 GTRI Assessment
 */
 
+#include <vector>
 #include <climits>
 #include <iostream>
 #include <string>
+#include "LinkedList.h"
 
 using namespace std;
 
 // prototypes
-int min_sum( int size, double *arr );
+int min_sum( vector<double> vect );
 
 
 int main ( int argc, char *argv[] )
 {
-    double* arr = new double[argc];
+    vector<double> arr;
 
-    // Takes CLI args and inserts into arror
+    // Takes CLI args and inserts into vector
     for ( int i = 1; i < argc; ++i )
-        arr[i] = atof(argv[i]) ;
+        arr.push_back ( atof(argv[i]) );
 
     cout << "Sum of the two lowest positive numbers from the array [";
-    for ( int i = 1; i < argc; i++ )
-        i == argc - 1 ? cout << arr[i] << ']': cout << arr[i] << ',';
+    for ( int i = 0; i < arr.size(); i++ )
+        i == arr.size() - 1 ? cout << arr[i] << ']': cout << arr[i] << ',';
 
-    cout << " is " << min_sum(argc, arr) << endl;
+    cout << " is " << min_sum(arr) << endl;
 
-    delete[] arr;
-    
     return 0;
 }
 
@@ -38,25 +38,24 @@ int main ( int argc, char *argv[] )
     Takes an array of integers as input.
     Returns sum of two smallest positive integers.
 */
-int min_sum( int size, double *arr )
+int min_sum( vector<double> vect )
 {
-    if ( size > 2 )
+    if ( vect.size() > 2 )
     {
         // initializes variables as max value of int as supported by C++
-        double min_1 = INT_MAX;
-        double min_2 = INT_MAX;
+        int min_1 = INT_MAX, min_2 = INT_MAX;
 
-        for ( int i = 0; i < size; i++ )
+        for ( int i = 0; i < vect.size(); i++ )
         {
             // if current element is smaller than our smallest stored variable
-            if ( arr[i] < min_1 && arr[i] > 0 )
+            if ( vect[i] < min_1 && vect[i] > 0 )
             {
                 min_2 = min_1;      // ovewrwrites second smallest with stored smallest
-                min_1 = arr[i];    // stores new smallest 
+                min_1 = vect[i];    // stores new smallest 
             }
             // if current element is smaller than stored second smallest but larger than stored smallest
-            else if ( arr[i] < min_2 && arr[i] != min_1 && arr[i] > 0 )
-                min_2 = arr[i];    // stores new second smallest
+            else if ( vect[i] < min_2 && vect[i] != min_1 && vect[i] > 0 )
+                min_2 = vect[i];    // stores new second smallest
 
         }
 
