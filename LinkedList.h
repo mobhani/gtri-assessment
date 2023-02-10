c57836b041e55cc4df25dc3d361654535abd6c11
mobhani <zainmobhani_b@outlook.com>
2023-02-10 18:25:57 -0500

    changes to LinkedList

--- LinkedList.h
+++ LinkedList.h
@@ -4,6 +4,7 @@
     exercise 2 and is not a fully-fledged Linked List.
 */
 
+#include <cstddef>
 using namespace std;
 
 class Node
@@ -12,40 +13,3 @@ class Node
     int data;
     struct Node *next;
 };
-
-/*
-    Returns true if elem present in List.
-    Returns false otherwise.
-*/
-bool checkDuplicate ( struct Node** head; int elem)
-{
-    struct Node *curr = head;
-    while ( curr != NULL && curr->data <= elem )
-    {
-        if ( curr->data == elem )
-            return true;
-        curr = curr->next;
-    }
-
-    return false;
-}
-
-/*
-    Inserts elements in ascending order.
-*/
-void insert ( struct Node** head, int node_data )
-{
-    // if data to insert isn't already present in List
-    if ( !checkDuplicate( head, node_data ) )
-    {
-        struct Node *curr = head;
-
-        while ( curr!= NULL && curr->data < node_data )
-            curr = curr->next;
-
-        struct Node* toInsert = new Node;
-
-
-    }
-
-}
